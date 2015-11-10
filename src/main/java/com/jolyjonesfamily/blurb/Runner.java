4f11d29a8ab98567281a8b9ee0da6c19ef543c35
Sam Jones <samguyjones@gmail.com>
2015-11-09 22:35:23 -0600

    Runner passes a unit test and command line options work.

--- src/main/java/com/jolyjonesfamily/blurb/Runner.java
+++ src/main/java/com/jolyjonesfamily/blurb/Runner.java
@@ -1,133 +1,126 @@
-//package com.jolyjonesfamily.blurb;
-//
-//import com.google.inject.Inject;
-//import com.google.inject.name.Named;
-//import com.jolyjonesfamily.blurb.selector.RandomSelector;
-//import org.apache.commons.cli.*;
-//import org.w3c.dom.Document;
-//import org.w3c.dom.Node;
-//
-//import javax.xml.parsers.DocumentBuilder;
-//import javax.xml.parsers.DocumentBuilderFactory;
-//import javax.xml.parsers.ParserConfigurationException;
-//import java.io.File;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import com.jolyjonesfamily.blurb.selector.Selector;
-//
+package com.jolyjonesfamily.blurb;
+
+import com.jolyjonesfamily.blurb.map.MapBlurb;
+import com.jolyjonesfamily.blurb.map.MapBlurbXML;
+import org.apache.commons.cli.*;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 ///**
 // * Created by samjones on 4/2/14.
 // */
-//public class Runner {
-//    private CategoryChooser baseCategory;
-//    private Node pattern;
-//    private static final char PARAM = 'p';
-//    private static final String PARAM_NAME = "parameters";
-//
-//    public static void main (String argv[]) {
-//        Runner myRunner = parseOptions(argv);
-//        if (myRunner != null) {
-//            System.out.println(myRunner.getOutput());
-//        }
-//    }
-//
+public class Runner {
+    private BlurbCatalog catalog;
+    private Map parameters;
+    private static Runner instance;
+    private static final char PARAM = 'p';
+    private static final String PARAM_NAME = "parameters";
+
+
+    public static void main(String argv[]) {
+        Runner myRunner = parseOptions(argv);
+        if (myRunner != null) {
+            System.out.println(myRunner.getOutput());
+        }
+    }
+
+    public static String run(String filename) throws Exception {
+        return getInstance(filename).getOutput();
+    }
+
+    //
 //    public CategoryChooser getBaseCategory() {
 //        return baseCategory;
 //    }
 //
-//    private static Options getOptions()
-//    {
-//        Options options = new Options();
-//        options.addOption(OptionBuilder.withArgName(PARAM_NAME)
-//            .hasArgs()
-//            .withValueSeparator('&')
-//            .withDescription("A set of key value pairs of options represented by key=value")
-//            .create(PARAM));
-//        return options;
-//    }
-//
-//    private static Runner parseOptions(String argv[])
-//    {
-//        Parser cliParser = new BasicParser();
-//        CommandLine cmd = null;
-//        try {
-//            cmd = cliParser.parse(getOptions(), argv);
-//        } catch (ParseException pe) {
-//            System.err.println("Error parsing command line.");
-//        }
-//        if (cmd.getArgs().length < 1) {
-//            HelpFormatter formatter = new HelpFormatter();
-//            formatter.printHelp("blurb XML_CONFIG", getOptions());
-//            return null;
-//        }
-//        return getInstance(cmd);
-//    }
-//
-//    private static Runner getInstance(CommandLine cmd)
-//    {
-//        Runner myRunner = new Runner(cmd.getArgs()[0]);
-//        if (cmd.hasOption(PARAM)) {
-//            Map<String, String> parameters = new HashMap<String, String>();
-//            String[] results = cmd.getOptionValues(PARAM);
-//            for(String myResult : results) {
-//                String[] twoParts = myResult.split("=");
-//                parameters.put(twoParts[0],twoParts[1]);
-//            }
-//            myRunner.baseCategory.setParams(parameters);
-//        }
-//        return myRunner;
-//    }
-//
-//    @Inject
-//    public Runner(@Named("Config") String filename, Selector numberGen)
-//    {
-//        Document xmlDoc = getDocument(filename);
-//        pattern = getPattern(xmlDoc);
-//        baseCategory = new CategoryChooser(pattern, numberGen);
-//    }
-//
-//    public Runner(String filename)
-//    {
-//        this(filename, new RandomSelector());
-//    }
-//
-//    public Node getPattern() {
-//        return pattern;
-//    }
-//
-//    public String getOutput() {
-//        return baseCategory.chooseEntry().getOutput();
-//    }
-//
-//    /**
-//     * Given a filename, return DOM Document object
-//     *
-//     * @param filename XML file
-//     * @return DOM document
-//     */
-//    private Document getDocument(String filename)
-//    {
-//        File blurbXml = new File(filename);
-//        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-//        DocumentBuilder dBuilder = null;
-//        try {
-//            dBuilder = dbFactory.newDocumentBuilder();
-//        } catch (ParserConfigurationException pce) {
-//            System.err.println("Parser configuration:  " + pce.getMessage());
-//            System.exit(1);
-//        }
-//        try {
-//            return dBuilder.parse(blurbXml);
-//        } catch (Exception e) {
-//            System.err.println("Parse Exception:  " + e.getMessage());
-//            System.exit(1);
-//        }
-//        return null;
-//    }
-//
-//    private final Node getPattern(Document xmlFile)
-//    {
-//        return xmlFile.getDocumentElement().getElementsByTagName("pattern").item(0);
-//    }
-//}
+    private static Options getOptions() {
+        Options options = new Options();
+        options.addOption(OptionBuilder.withArgName(PARAM_NAME)
+                .hasArgs()
+                .withValueSeparator('&')
+                .withDescription("A set of key value pairs of options represented by key=value")
+                .create(PARAM));
+        return options;
+    }
+
+    private static Runner parseOptions(String argv[]) {
+        Parser cliParser = new BasicParser();
+        CommandLine cmd = null;
+        try {
+            cmd = cliParser.parse(getOptions(), argv);
+        } catch (ParseException pe) {
+            System.err.println("Error parsing command line.");
+        }
+        if (cmd.getArgs().length < 1) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("blurb XML_CONFIG", getOptions());
+            return null;
+        }
+        try {
+            return getInstance(cmd);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+    }
+
+    //
+    private static Runner getInstance(CommandLine cmd) throws Exception {
+        instance = getInstance(cmd.getArgs()[0]);
+        if (cmd.hasOption(PARAM)) {
+            Map<String, String> parameters = new HashMap<String, String>();
+            String[] results = cmd.getOptionValues(PARAM);
+            for (String myResult : results) {
+                String[] twoParts = myResult.split("=");
+                parameters.put(twoParts[0], twoParts[1]);
+            }
+            instance.setParameters(parameters);
+        }
+        return instance;
+    }
+
+    private static Runner getInstance() throws Exception {
+        if (instance == null) {
+            instance = new Runner();
+        }
+        instance.setParameters(new HashMap());
+        return instance;
+    }
+
+    public static Runner getInstance(MapBlurb map) throws Exception {
+        return getInstance().setCatalog(new BlurbCatalog(map));
+    }
+
+    public static Runner getInstance(String filename) throws Exception
+    {
+        return getInstance(new MapBlurbXML(new File(filename)));
+    }
+
+    public Map getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Map parameters) {
+        this.parameters = parameters;
+    }
+
+    public Runner() throws Exception
+    {
+//        setCatalog(new BlurbCatalog(new MapBlurbXML(new File(filename))));
+    }
+
+    public BlurbCatalog getCatalog() {
+        return catalog;
+    }
+
+    public Runner setCatalog(BlurbCatalog catalog) {
+        this.catalog = catalog;
+        return instance;
+    }
+
+    public String getOutput() {
+        return catalog.fetch(getParameters()).getOutput();
+    }
+}
--- src/test/java/com/jolyjonesfamily/blurb/RunnerTest.java
+++ src/test/java/com/jolyjonesfamily/blurb/RunnerTest.java
@@ -1,14 +1,31 @@
-//package com.jolyjonesfamily.blurb;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-///**
-// * Created by samjones on 1/18/15.
-// */
-//public class RunnerTest extends BlurbTest {
-//    @Test public void output()
-//    {
-//        Assert.assertEquals("Faith is the root of despair.", setupRunner.getOutput());
-//    }
-//}
+package com.jolyjonesfamily.blurb;
+
+import com.jolyjonesfamily.blurb.selector.TestSelector;
+import com.jolyjonesfamily.blurb.selector.Selector;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+* Created by samjones on 1/18/15.
+*/
+public class RunnerTest extends BlurbTest {
+    @Test public void output()
+    {
+        Runner runner;
+        try {
+            runner = Runner.getInstance(getMap());
+        } catch (Exception e) {
+            Assert.fail();
+            return;
+        }
+        Selector testSelector = new TestSelector(new Stack<Integer>(){{
+            push(4);
+            push(3);
+            push(0);
+        }});
+        CategorySwitch.setGenerator(testSelector);
+        Assert.assertEquals("Faith is the root of despair.", runner.getOutput());
+    }
+}
